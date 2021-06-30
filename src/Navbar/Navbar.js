3e3cd10625c1ea25ee63758bcab0bf0842fc6625
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-06-30 13:35:44 +0200

    react exo 5

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+
+export default function Navbar(){
+    return(
+        <nav>
+            <ul className="liste">
+                <li className="items">Accueil</li>
+                <li className="items">Service</li>
+                <li className="items">Contact</li>
+            </ul>
+            <button className="btn">BTN</button>
+        </nav>
+    )
+}
+
